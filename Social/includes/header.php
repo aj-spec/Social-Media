693dd875d36921f30be4034509bb03ebbbf18028
Devansh Kumar <69133618+itsdevansh@users.noreply.github.com>
2020-08-29 18:52:32 +0530

    Add files via upload
    till vid 59

--- Social/includes/header.php
+++ Social/includes/header.php
@@ -1,7 +1,8 @@
-<?php
+<?php  
 require 'config/config.php';
 
-if(isset($_SESSION['username'])) {
+
+if (isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
 	$user = mysqli_fetch_array($user_details_query);
@@ -14,32 +15,31 @@ else {
 
 <html>
 <head>
-     <title>Enroute</title>
+	<title>Enroute</title>
 
-     <!---Javascript---->
+	<!-- Javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
 
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-     <script src="Assets/js/bootstrap.js"></script>
-     
-     <!---CSS--->
-
-     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-     <link rel="stylesheet" type="text/css" href="Assets/css/bootstrap.css">
-     <link rel="stylesheet" type="text/css" href="Assets/css/style.css">
+	<!-- CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 </head>
 <body>
-	<div class="top_bar">
+
+	<div class="top_bar"> 
+
 		<div class="logo">
 			<a href="index.php">Enroute</a>
-			
 		</div>
 
 		<nav>
 			<a href="<?php echo $userLoggedIn; ?>">
 				<?php echo $user['first_name']; ?>
 			</a>
-			<a href="#">
+			<a href="index.php">
 				<i class="fa fa-home fa-lg"></i>
 			</a>
 			<a href="#">
@@ -57,10 +57,12 @@ else {
 			<a href="includes/handlers/logout.php">
 				<i class="fa fa-sign-out fa-lg"></i>
 			</a>
+
+
+
 		</nav>
-		
+
 	</div>
 
 
 	<div class="wrapper">
-
