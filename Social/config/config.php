889133f9a9dbe05ceff6a197054ec5726de25a18
Devansh Kumar <69133618+itsdevansh@users.noreply.github.com>
2020-09-03 23:20:05 +0530

    Add files via upload

--- Social/config/config.php
+++ Social/config/config.php
@@ -1,10 +1,14 @@
 <?php
-ob_start(); // Turns on output buffering
+ob_start(); //Turns on output buffering 
 session_start();
 
-$timezone = date_default_timezone_set("Asia/Dili");
-$con = mysqli_connect("localhost", "root", "", "social");
-if(mysqli_connect_errno()){
+$timezone = date_default_timezone_set("Europe/London");
+
+$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+
+if(mysqli_connect_errno()) 
+{
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
+
 ?>
--- Social/profile.php
+++ Social/profile.php
@@ -1,12 +1,17 @@
-
-<?php
+<?php 
 include("includes/header.php");
-?>
-
-
-<div class="main_column column">
-	This is a profile page
+ ?>
 	
-</div>
+
+	<div class="main_column column">
+		This is a profile page!
+
+
+	</div>
+
+
+
+
+	</div>
 </body>
 </html>
