e784847cffb73dbe884af8293ce6378fdf2144c4
Ayushi Jain <68581010+aj-spec@users.noreply.github.com>
2020-09-23 17:41:22 +0530

    Add files via upload

--- Social/Assets/js/register.js
+++ Social/Assets/js/register.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
+
 	//On click signup, hide login and show registration form
 	$("#signup").click(function() {
-		$("#first").slideUp("slow", function() {
+		$("#first").slideUp("slow", function(){
 			$("#second").slideDown("slow");
-
 		});
-
 	});
 
-	//On click signup, hide login and show registration form
-	$("#signin").click(function(){
+	//On click signup, hide registration and show login form
+	$("#signin").click(function() {
 		$("#second").slideUp("slow", function(){
 			$("#first").slideDown("slow");
-
 		});
-
 	});
 
 
--- Social/includes/header.php
+++ Social/includes/header.php
@@ -1,7 +1,11 @@
-<?php
+<?php  
 require 'config/config.php';
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
+include("includes/classes/Message.php");
 
-if(isset($_SESSION['username'])) {
+
+if (isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
 	$user = mysqli_fetch_array($user_details_query);
@@ -14,32 +18,38 @@ else {
 
 <html>
 <head>
-     <title>Enroute</title>
+	<title>Enroute</title>
 
-     <!---Javascript---->
+	<!-- Javascript -->
 
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-     <script src="Assets/js/bootstrap.js"></script>
-     
-     <!---CSS--->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="Assets/js/bootstrap.js"></script>
+    <script src="Assets/js/bootbox.min.js"></script>
+	<script src="Assets/js/social.js"></script>
+	<script src="Assets/js/jquery.jcrop.js"></script>
+	<script src="Assets/js/jcrop_bits.js"></script>
+	
 
-     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-     <link rel="stylesheet" type="text/css" href="Assets/css/bootstrap.css">
-     <link rel="stylesheet" type="text/css" href="Assets/css/style.css">
+	<!-- CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="Assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="Assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="Assets/css/jquery.Jcrop.css">
 
 </head>
 <body>
-	<div class="top_bar">
+
+	<div class="top_bar"> 
+
 		<div class="logo">
 			<a href="index.php">Enroute</a>
-			
 		</div>
 
 		<nav>
 			<a href="<?php echo $userLoggedIn; ?>">
 				<?php echo $user['first_name']; ?>
 			</a>
-			<a href="#">
+			<a href="index.php">
 				<i class="fa fa-home fa-lg"></i>
 			</a>
 			<a href="#">
@@ -48,7 +58,7 @@ else {
 			<a href="#">
 				<i class="fa fa-bell fa-lg"></i>
 			</a>
-			<a href="#">
+			<a href="requests.php">
 				<i class="fa fa-users fa-lg"></i>
 			</a>
 			<a href="#">
@@ -57,10 +67,12 @@ else {
 			<a href="includes/handlers/logout.php">
 				<i class="fa fa-sign-out fa-lg"></i>
 			</a>
+
+
+
 		</nav>
-		
+
 	</div>
 
 
 	<div class="wrapper">
-
